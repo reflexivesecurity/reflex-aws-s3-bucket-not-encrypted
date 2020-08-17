1136d4ee8a0ecf4b5f538126d48d82b452f680f6
rjulian <richard@rjulian.net>
2020-08-16 22:03:05 -0700

    Add in assume_role tf code

---
+++ terraform/assume_role/assume_role.tf
@@ -0,0 +1,24 @@
+data "aws_caller_identity" "current" {}
+module "assume_role" {
+  source = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/sqs_lambda/modules/iam_assume_role?ref=multi-account"
+
+  function_name             = "S3BucketNotEncrypted"
+  custom_lambda_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:GetEncryptionConfiguration",
+        "s3:PutEncryptionConfiguration"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+
+  lambda_execution_role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/ReflexS3BucketNotEncryptedLambdaExecution"
+
+}
